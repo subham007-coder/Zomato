238be1bf7c2a7864b524eda0854dbee9cf859fe8
subham007-coder <dsubham109@gmail.com>
2024-03-09 12:18:53 +0530

    add samplae data

---
+++ init/data.js
@@ -0,0 +1,137 @@
+const sampleListings = [
+    {
+      title: "Arsalan",
+      description: "Biryani, North Indian, Mughlai, Kebab, Rolls, Seafood",
+      image: {
+        filename: "listingimage",
+        url: "https://b.zmtcdn.com/data/pictures/chains/7/20797/7c440b99b4140cce88505e64bb690b0f_o2_featured_v2.jpg?output-format=webp",
+      },
+      time: "33 min",
+      price: 250,
+      location: "Park Street Area, Kolkata",
+    },
+    {
+      title: "Al Baik Fast Food Corner",
+      description:
+        "Biryani, Rolls, North Indian, Chinese, Kebab, Fast Food, Street Food, Roast Chicken",
+      image: {
+        filename: "listingimage",
+        url: "https://b.zmtcdn.com/data/pictures/chains/6/18560756/582a2813f08cd285f9941587f603c68f_o2_featured_v2.jpg?output-format=webp",
+      },
+      time: "30 min",
+      price: 250,
+      location: "Bara Bazar, Kolkata",
+    },
+    {
+      title: "Lazeez Biryani",
+      description: "Biryani, North Indian",
+      image: {
+        filename: "listingimage",
+        url: "https://b.zmtcdn.com/data/pictures/6/18761176/faa954598a51b5d2ead55d405a8f14b2_o2_featured_v2.jpg?output-format=webp",
+      },
+      time: "17 min",
+      price: 250,
+      location: "Entally, Kolkata",
+    },
+    {
+      title: "La Pino'z Pizza",
+      description: "Pizza, Pasta, Italian, Fast Food",
+      image: {
+        filename: "listingimage",
+        url: "https://b.zmtcdn.com/data/pictures/chains/3/19264953/eac5d48527700f6ea1bcfbb2094456d0.jpg?fit=around|771.75:416.25&crop=771.75:416.25;*,*",
+      },
+      time: "34 min",
+      price: 250,
+      location: "Kankurgachi, Kolkata",
+    },
+    {
+      title: "KFC",
+      description: "Fast Food, Burger, Biryani, Desserts, Beverages",
+      image: {
+        filename: "listingimage",
+        url: "https://b.zmtcdn.com/data/pictures/chains/7/20287/b95de2b33a9e18dcdb2da7a179202dd8.jpg?fit=around|771.75:416.25&crop=771.75:416.25;*,*",
+      },
+      time: "28 min",
+      price: 250,
+      location: "Sealdah Area, Kolkata",
+    },
+    {
+      title: "Oudh 1590",
+      description:
+        "North Indian, Awadhi, Mughlai, Kebab, Biryani, Desserts, Beverages.",
+      image: {
+        filename: "listingimage",
+        url: "https://b.zmtcdn.com/data/pictures/chains/3/23273/e13f6132a65233d63e9dbe6e6cda910f_featured_v2.jpg?fit=around|771.75:416.25&crop=771.75:416.25;*,*",
+      },
+      time: "38 min",
+      price: 250,
+      location: "Paikpara, Kolkata",
+    },
+  
+    {
+      title: "WOW! Momo",
+      description:
+        "Momos, Fast Food, Chinese, Tibetan, Sichuan, Beverages, Desserts",
+      image: {
+        filename: "listingimage",
+        url: "https://b.zmtcdn.com/data/pictures/chains/0/21060/7a3d71e5a94b487b4b61fb5e374f6b56.jpg?fit=around|771.75:416.25&crop=771.75:416.25;*,*",
+      },
+      time: "30 min",
+      price: 150,
+      location: "Sealdah Area, Kolkata",
+    },
+  
+    {
+      title: "Pizza Hut",
+      description:
+        "Pizza, Pasta, Italian, Fast Food",
+      image: {
+        filename: "listingimage",
+        url: "https://b.zmtcdn.com/data/pictures/chains/7/20407/a32bffd7a846b6cfd5475ce56a6daca1.jpg?output-format=webp&fit=around|771.75:416.25&crop=771.75:416.25;*,*",
+      },
+      time: "29 min",
+      price: 100,
+      location: "Esplanade, Kolkata",
+    },
+    {
+      title: "Chowman",
+      description:
+        "Chinese, Seafood, Sichuan, Beverages",
+      image: {
+        filename: "listingimage",
+        url: "https://b.zmtcdn.com/data/pictures/chains/5/21495/8c4620d51de56bb45a8462a2baca5a96.jpg?fit=around|771.75:416.25&crop=771.75:416.25;*,*",
+      },
+      time: "30 min",
+      price: 150,
+      location: "Hatibagan, Kolkata",
+    },
+  
+    {
+      title: "Domino's Pizza",
+      description:
+        "Chinese, Seafood, Sichuan, Beverages",
+      image: {
+        filename: "listingimage",
+        url: "https://b.zmtcdn.com/data/pictures/chains/2/20172/0b2779d59a54d0b06e9f9ea0a94639bb.jpg?fit=around|771.75:416.25&crop=771.75:416.25;*,*",
+      },
+      time: "33 min",
+      price: 200,
+      location: "Bara Bazar, Kolkata",
+    },
+    {
+      title: "Kasturi Restaurant",
+      description:
+        "Bengali, Bangladeshi, Seafood, Biryani",
+      image: {
+        filename: "listingimage",
+        url: "https://b.zmtcdn.com/data/pictures/chains/6/21576/0d48f9b50a9f8226ca2791e4f45823b9.jpg?fit=around|771.75:416.25&crop=771.75:416.25;*,*",
+      },
+      time: "54 min",
+      price: 200,
+      location: "New Market Area, Kolkata",
+    },
+  
+  ];
+  
+  module.exports = { data: sampleListings };
+  
