ba788f079e7f831c5f411c09bb5fae4c68f63ce5
subham007-coder <dsubham109@gmail.com>
2024-03-09 13:53:49 +0530

    11 data add

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const Listing = require("./models/listings.js");
+const path = require("path");
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/Zomato";
 
@@ -17,24 +18,20 @@ async function main() {
   await mongoose.connect(MONGO_URL);
 }
 
-app.get("/", (req, res) => {
-  res.send("hii day1");
+// set the view engine to ejs
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+app.get("/", async (req, res) => {
+  const allListings = await Listing.find({});
+  res.render("./listings/index.ejs", { allListings });
 });
 
-
-// sampale data
-// app.get("/test", async (req, res) => {
-//   let sampleListing = new Listing({
-//     title: "biriyani",
-//     description: "North Indian",
-//     price: 200,
-//   });
-//   await sampleListing.save();
-//   console.log("sample was saved");
-//   res.send("testing was done");
-// });
-
-
+// index route
+app.get("/listings", async (req, res) => {
+  const allListings = await Listing.find({});
+  res.render("./listings/index.ejs", { allListings });
+});
 
 app.listen(8080, () => {
   console.log("server is start on");
